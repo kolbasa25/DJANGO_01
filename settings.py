76be94dd21a0fee408742c81a7a51f3851db998d
kolbasa25 <63467154+kolbasa25@users.noreply.github.com>
2026-03-10 23:38:59 +1000

    main

---
+++ settings.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+    }
+}
+
+INSTALLED_APPS = ['datacenter']
+
+SECRET_KEY = 'REPLACE_ME'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_TZ = True
